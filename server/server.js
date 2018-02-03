4e58e30d47f9242c631e7cfc1d0bc6761f5b66d1
Yao <151736726@qq.com>
2018-02-03 13:19:17 +0800

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const bcrypt = require('bcryptjs');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -112,6 +113,18 @@ app.post('/users', (req, res) => {
 // 请求'/user/me'时 额外添加Headers内Key: x-auth; Value: '注册用户时存放在header里的token'
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => { //login验真后，res.header加入token
+  let body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 //event监听3000端口有没有被req
